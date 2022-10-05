55cdd536b7e7a6c6cbfe1ed243080db68ce4b8a1
Alan Vilalte <alan9106@gmail.com>
2022-10-05 20:33:16 -0300

    :hammer: Project structuring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AddBook from "./pages/AddBook";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./styles/styles.css";
 
 function App() {
   return (
-    <div className="App">   
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home title="Library" />} />
+          <Route path="/add-book" element={<AddBook title="Add new book" />} />
+          <Route path="/sign-up" element={<AddBook title="Add new book" />} />
+        </Routes>
+        <Footer />
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="divRandom">QUIDBWQ</div>
+      Home
+    </div>
+  );
+};
+
+export default Home;
