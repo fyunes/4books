aa99f561c47c06e10a93cbd2cd59f1103405065c
Fabricio Hang <saveasfabri@gmail.com>
2022-10-07 20:11:01 -0300

    :sparkles: feat: Creation of logo and favicon and start to develop SignUP page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./components/Footer";
 import AddBook from "./pages/AddBook";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./styles/styles.css";
+import SignUpForm from "./components/Form";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
@@ -14,7 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home title="Library" />} />
           <Route path="/add-book" element={<AddBook title="Add new book" />} />
-          <Route path="/sign-up" element={<AddBook title="Add new book" />} />
+          <Route path="/sign-up" element={ <SignUpForm title="Sign Up" /> } />
+          <Route path="/sign-in" element={ <SignIn title="Sign In" /> } />
         </Routes>
         <Footer />
       </div>
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../../components/Form'
+
+const SignUpForm = () => {
+  return (
+    <div> 
+      <h2>Sign Up Form</h2>
+      <div>
+        <Form />
+      </div>
+    </div>
+  )
+};
+
+export default SignUpForm;
