2184cc5028a821ac01ef6fb29365cf21b1f19410
Alan Vilalte <alan9106@gmail.com>
2022-10-17 01:32:03 -0300

    :sparkles: Web view carousel

--- src/components/homeCarousel.js
+++ src/components/homeCarousel.js
@@ -8,24 +8,11 @@ import {
   useCarousel,
 } from "chakra-framer-carousel";
 
-// const images = [
-//   "../assets/images/carousel/1.jpg",
-//   "../assets/images/carousel/2.jpg",
-//   "../assets/images/carousel/3.jpg",
-//   "../assets/images/carousel/4.jpg",
-//   "../assets/images/carousel/5.jpg",
-//   "../assets/images/carousel/6.jpg",
-//   "../assets/images/carousel/7.jpg",
-// ];
-
 const images = [
-  "https://images.unsplash.com/photo-1606567595334-d39972c85dbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=",
-  "https://images.unsplash.com/photo-1572402230267-f3e267c1e5a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-  "https://images.unsplash.com/photo-1497206365907-f5e630693df0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-  "https://images.unsplash.com/photo-1551085254-e96b210db58a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-  "https://images.unsplash.com/photo-1552728089-57bdde30beb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-  "https://images.unsplash.com/photo-1555169062-013468b47731?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
-  "https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+  "https://github.com/avilalte/skillfactory-third-project/blob/dev/src/assets/images/fr.jpg?raw=true",
+  "https://github.com/avilalte/skillfactory-third-project/blob/dev/src/assets/images/al.jpg?raw=true",
+  "https://github.com/avilalte/skillfactory-third-project/blob/dev/src/assets/images/Me.jpg?raw=true",
+  "https://github.com/avilalte/skillfactory-third-project/blob/dev/src/assets/images/fa.jpg?raw=true",
 ];
 
 function Card({ image, index }) {
@@ -33,28 +20,32 @@ function Card({ image, index }) {
   const isCenter = position === "center";
   return (
     <Flex
-      minH={
+      h={
         isCenter
-          ? { base: "300px", sm: "400px", md: "550px", lg: "400px" }
-          : { base: "200px", sm: "150px", md: "250px", lg: "350px" }
+          ? { base: "300px", md: "400px", xl: "450px" }
+          : { base: "280px", md: "320px", xl: "400px" }
       }
-      boxSize={
+      w={
         isCenter
-          ? { base: "200px", sm: "300px", md: "400px", lg: "500px" }
-          : { base: "200px", sm: "150px", md: "250px", lg: "500px" }
+          ? { base: "200px", md: "280px", xl: "330px" }
+          : { base: "180px", md: "220px", xl: "290px" }
       }
       boxShadow="lg"
       as="button"
       onClick={onClickHandler}
     >
       <Image
-        minH={{ base: "300px", sm: "400px", md: "550px", lg: "400px" }}
-        src={image}
-        boxSize={
+        h={
           isCenter
-            ? { base: "200px", sm: "300px", md: "400px", lg: "500px" }
-            : { base: "100px", sm: "150px", md: "250px", lg: "300px" }
+            ? { base: "300px", md: "400px", xl: "450px" }
+            : { base: "280px", md: "320px", xl: "400px" }
         }
+        w={
+          isCenter
+            ? { base: "200px", md: "280px", xl: "330px" }
+            : { base: "180px", md: "220px", xl: "290px" }
+        }
+        src={image}
         objectFit=""
         objectPosition="center center"
         borderRadius={10}
@@ -70,14 +61,14 @@ function Toolbar() {
       <HStack>
         <Button
           colorScheme="yellow"
-          w={{ base: "40px", sm: "55px", md: "115px" }}
+          w={{ base: "40px", sm: "55px", md: "65px" }}
           onClick={onPrevious}
         >
           <ArrowBackIcon />
         </Button>
         <Button
           colorScheme="yellow"
-          w={{ base: "40px", sm: "55px", md: "115px" }}
+          w={{ base: "40px", sm: "55px", md: "65px" }}
           onClick={onNext}
         >
           <ArrowForwardIcon />
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,48 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { useEffect } from "react";
 import HomeCarousel from "../../components/homeCarousel";
+import useWindowSize from "../../customHooks/useWindowSize";
 
 const Home = ({ title }) => {
   useEffect(() => {
     document.title = title;
   });
+
+  const size = useWindowSize();
+  console.log("size  ", size.width);
+
   return (
     <Box
-      w="80%"
+      w={{ base: "100%", lg: "70%" }}
       borderWidth={2}
       borderColor="orange.100"
       borderRadius={20}
       py={2}
       my={2}
-      bg='maroon.op'
+      bg="maroon.op"
       display="flex"
-      alignSelf='center'
+      alignSelf="center"
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
       color="beige.100"
     >
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        gap='120'
-        flexDir="column"
-      >
-        <HomeCarousel />
-      </Flex>
+      {size.width > 600 ? (
+        <Flex
+          maxW="100%"
+          // borderWidth={1}
+          // borderColor="red"
+          justifyContent="center"
+          alignItems="center"
+          gap="5"
+          mt={2}
+          flexDir="column"
+        >
+          <HomeCarousel />
+        </Flex>
+      ) : (
+        "Something else"
+      )}
     </Box>
   );
 };
