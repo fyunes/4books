028d39588ba9e78ccfcca46bff889b73c549c2dc
Alan V <lavtiel@gmail.com>
2024-04-06 01:25:33 -0300

    change Logo & title images source

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -12,14 +12,14 @@ const Logo = () => {
       <Box>
         <Image
           w={{ base: "80px", sm: "100px", md: "100px" }}
-          src="https://media.discordapp.net/attachments/997992289425182791/1031176706343587840/logo_colors.png?width=468&height=468"
+          src="../assets/images/logo_colors.png"
           alt="Logo"
         />
       </Box>
       <Box display="flex" alignItems="center">
         <Image
           w={{ base: "170px", sm: "200px", md: "220px" }}
-          src="https://media.discordapp.net/attachments/997992289425182791/1031177404992991242/logotipo-4b.mixto.png"
+          src="../assets/images/logtipo-4b.mixto.png"
           alt="Name App"
         />
       </Box>
